c4fbdffbbe329aac02fb49375add48d5ecb94a88
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2024-12-21 14:11:29 +0000

    [finish]bookers2を作成しよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,36 @@
 class BooksController < ApplicationController
+
+  def new
+    @Book = Book.new
+  end
+
+  # 投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+    @books = Book.page(params[:page])
+  end
+
+  def show
+    @book = find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy 
+    redirect_to '/books'
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:shop_name, :image, :caption)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    def show
+      @user = User.find(params[:id])
+      @users = @user.books    
+    end
+  
+    def edit
+      @user = User.find(params[:id])
+    end
+
 end
